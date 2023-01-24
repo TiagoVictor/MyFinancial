783d23c95edca9c008ca1bf413a6c7a0b1323625
Tiago Victor de Oliveira <tiagovictordeoliveira@gmail.com>
2023-01-23 23:09:56 -0300

    Criando a UserService + IUserService

---
+++ MyFinancial/Service/UserService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using MyFinancial.Data.DataModel;
+using MyFinancial.Data.Repository.Interface;
+using MyFinancial.Dto;
+using MyFinancial.Service.Interface;
+
+namespace MyFinancial.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task DeleteUserAsync(int id)
+        {
+            await _userRepository.DeleteUser(id);
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _userRepository.GetAllUsersAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _userRepository.GetUserAsync(id);
+        }
+
+        public async Task SaveUserAsync(UserDto dto)
+        {
+            await _userRepository.SaveUserAsync(dto);
+        }
+
+        public void UpdateUser(UserDto dto)
+        {
+            _userRepository.UpdateUser(dto);
+        }
+    }
+}
