a14ca4b1e587d40e59e3d5a8d748949a034bb24c
Tiago Victor de Oliveira <tiagovictordeoliveira@gmail.com>
2023-01-23 23:04:00 -0300

    Criando UserRepository + IUserRepository

---
+++ MyFinancial/Data/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using MyFinancial.Data.DataModel;
+using MyFinancial.Dto;
+
+namespace MyFinancial.Data.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        Task SaveUserAsync(UserDto dto);
+        void UpdateUser(UserDto dto);
+        Task DeleteUser(int id);
+        Task<User> GetUserAsync(int id);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+    }
+}
