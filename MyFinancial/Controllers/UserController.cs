a305c2d5ca1efa97e6722b9d1dbe862803d44617
Tiago Victor <tiagovictordeoliveira@gmail.com>
2023-01-24 22:48:14 -0300

    Criando UserController com a view Index basicas

---
+++ MyFinancial/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFinancial.Service.Interface;
+
+namespace MyFinancial.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
